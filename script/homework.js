699d0a49d8aabb0fd36445ec16d29dce3a9a828f
Max Strikanov <maxvell.88@mail.ru>
2020-08-13 19:13:08 +0300

    first commit

---
+++ script/homework.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const num = 266219;
+let numArr = Array.from( num.toString() );
+
+console.log( numArr );
+
+let product = numArr.reduce( ( prev, curr ) => { 
+    return prev * curr 
+});
+
+console.log( product );
+
+const exponent = ( product, n ) => {
+
+    let result = 1;
+    
+    for (let i = 0; i < n; i++) {
+      result *= product;
+    }
+    return result;
+}
+
+console.log( exponent( product, 3 ) );
+console.log( exponent( product, 3 ).toString().slice(0,2) ); 
+
+
+
+
+
+
+
